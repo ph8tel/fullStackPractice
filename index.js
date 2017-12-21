b8368a015a55974cd80b7de03318f922fdaa560f
Joe Jackson <ph8tel@gmail.com>
2017-12-21 10:33:24 -0800

    basic server in and working

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const http = require('http')
+const express = require('express')
+const path = require('path')
+const port = 4000;
+
+const app = new express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, (req, res) => console.log('Runnin on ', port))
